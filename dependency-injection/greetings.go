f1fc11c856f262575072ee74f64d525fa49d2ce8
nicolaslema <nicolemadev@gmail.com>
2024-06-11 19:29:42 -0300

    Dependency injection & mocking

---
+++ dependency-injection/greetings.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(w io.Writer, name string) {
+	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
